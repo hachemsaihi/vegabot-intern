0c04d1504cd24c5eb496e57c34de261c4450fb62
hachemsaihi <hachem.saihi@insat.ucar.tn>
2023-07-21 09:57:20 +0100

    flask

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import openai
-from typing import Union
-from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.prompt import PromptTemplate
 from dotenv import load_dotenv, find_dotenv
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
-from fastapi import FastAPI
+from flask import Flask, jsonify, request
 
-SQLALCHEMY_DATABASE_URL = "postgresql://default:65EvzyobfYsR@http://ep-spring-pond-821996-pooler.eu-central-1.postgres.vercel-storage.com//verceldb"
-
-app=FastAPI()
+app = Flask(__name__)
 import warnings
 warnings.filterwarnings('ignore')
 _=load_dotenv(find_dotenv())
 openai.api_key=os.environ['OPENAI_API_KEY']
-name='hachem saihi'
-age='22'
-gender='male'
-conditions='arthritis'
-allergies='dog allergy'
-bloodtype='A+'
 chat=ChatOpenAI(temperature=0)
 memory=ConversationBufferMemory()
 template="""You are a very smart medical doctor named VegaBot. \
@@ -37,11 +24,12 @@ current conversation : \
 {history}
 Human : {input} \
 AI assistant :"""
-memory.save_context({'input':f"my name is {name}. my age is {age}. my gender is {gender}. my conditions are{conditions}. my allergies are {allergies}. my bloodtype is {bloodtype}."},{'output':'okay.'})
+#memory.save_context({'input':f"my name is {name}. my age is {age}. my gender is {gender}. my conditions are{conditions}. my allergies are {allergies}."},{'output':'okay.'})
 prompt=PromptTemplate(input_variables=['history','input'],template=template)
 conversation=ConversationChain(prompt=prompt,llm=chat,memory=memory,verbose=False)
-
-@app.post('/response')
-def getResponse(ch : str):
-    return {'response' : conversation.predict(input=ch)}
-
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_input = request.form['user_input']
+    bot_response = conversation.predict(input=user_input)
+    response_data = {'response': bot_response}
+    return jsonify(response_data)
